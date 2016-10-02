994952c5ea376b3389d3ef53b78b90c764df3d7a
Ubuntu <ubuntu@ip-172-31-33-112.us-west-2.compute.internal>
2016-10-02 23:41:58 +0000

    make it better

---
+++ index.js
@@ -0,0 +1,66 @@
+/*
+var fs = require('fs');
+var http = require('http');
+var port = Number(process.env.PORT || 8080);
+
+var fundstarter = function(request, response){
+	var buffer = fs.readFileSync('index.html', 'utf8');
+	response.writeHead(200, {'Content-type': 'text/html'});
+	response.end(buffer);
+};
+
+var server = http.createServer(fundstarter);
+server.listen(port);
+*/
+
+/*
+var fs = require('fs');
+var http = require('http');
+var port = Number(process.env.PORT || 8080);
+
+var fundstarter = http.createServer(function(request, response){
+	response.writeHead(200, {'Content-type': 'text/html'});
+	fs.readFile('index.html', 'utf8', function(error, data){
+		if(error){
+			throw error;
+		}
+		response.write(data);
+		response.end();
+	});
+}).listen(port);
+*/
+
+
+
+var fs = require('fs');
+var http = require('http');
+var port = process.env.PORT || 8080;
+var buffer = new Buffer(2048);
+
+function find(callback){
+	fs.open('index.html', 'r+', 
+		function(error, fd){
+			if(error){
+				return callback(error);
+			}
+			fs.read(fd, buffer, 0, buffer.length, 0,
+				function(error, bytes){
+					if(error){
+						return callback(error);
+					}
+					callback(null, buffer.slice(0, bytes));
+				}
+			)
+		}
+	)
+}
+
+
+find(function(error, data){
+	function onRequest(request, response){
+		response.writeHead(200, {'Content-type': 'text/html'});
+		response.write(data);
+		reponse.end();
+	}
+	http.createServer(onRequest).listen(port);
+})
--- others/index.js
+++ others/index.js
@@ -1,32 +1,31 @@
-var http = require('http');
 var fs = require('fs');
+var ptcl = require('http');
 var port = process.env.PORT || 8080;
-var requestlistener = function(request, response)
-{
-	fs.stat('index.html', function(error, stats)
-	{
-		if(error)
-		{
+var find = function(request, response){
+var server = ptcl.createServer(find);
+
+	fs.stat('index.html', function(error, stats){
+		if(error){
 			return console.log(error);
 		}
-		fs.open('index.html', 'r', function(error, fd)
-		{
+		fs.open('index.html', 'r', function(error, fd){
 			if(error){
 				return console.log(error)
 		}
 		var bufferSize = stats.size;
 		var buffer = new Buffer(bufferSize);
 
-		fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer)
-		{
-			var data = buffer.toString('utf8');
-			response.writeHeader(200, {'Content-Type': 'text/html'});
-			response.write(data);
-			response.end();
-		});
-	});
-});
+		fs.read(fd, buffer, 0, buffer.length, null, 
+			function(error, bytesRead, buffer){
+				var data = buffer.toString('utf8');
+				response.writeHeader(200, {'Content-Type': 'text/html'});
+				response.write(data);
+				response.end();
+			}
+			);
+		}
+		);
+	}
+	);
 };
-
-var server = http.createServer(requestlistener);
 server.listen(port);
