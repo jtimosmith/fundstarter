3fbf3e894240459aa1ed437691df1630897ba275
Ubuntu <ubuntu@ip-172-31-33-112.us-west-2.compute.internal>
2016-10-03 00:07:48 +0000

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = Number(process.env.PORT || 8080);
 
 var fundstarter = function(request, response){
 	var buffer = fs.readFileSync('index.html', 'utf8');
-	response.writeHead(200, {'Content-type': 'text/html'});
+	response.writeHead(200, {"Content-type": "text/html"});
 	response.end(buffer);
 };
 
@@ -19,7 +19,7 @@ var http = require('http');
 var port = Number(process.env.PORT || 8080);
 
 var fundstarter = http.createServer(function(request, response){
-	response.writeHead(200, {'Content-type': 'text/html'});
+	response.writeHead(200, {"Content-type": "text/html"});
 	fs.readFile('index.html', 'utf8', function(error, data){
 		if(error){
 			throw error;
@@ -37,30 +37,25 @@ var http = require('http');
 var port = process.env.PORT || 8080;
 var buffer = new Buffer(2048);
 
-function find(callback){
-	fs.open('index.html', 'r+', 
-		function(error, fd){
-			if(error){
-				return callback(error);
-			}
-			fs.read(fd, buffer, 0, buffer.length, 0,
-				function(error, bytes){
-					if(error){
-						return callback(error);
-					}
-					callback(null, buffer.slice(0, bytes));
+var find = function(request, response){
+	response.writeHead(200, {"Content-type": "text/html"});
+	fs.exists('index.html', function(exist){
+		if(exist){
+			fs.stat('index.html', 
+				function(error, stats){
+					fs.open('index.html', 'r', 
+						function(error, fd){
+							var buffer = new Buffer(stats.size);
+							fs.read(fd ,buffer, 0, buffer.length, 0, 
+								function(error, bytes, buffer){
+									response.write(buffer.toString('utf8', 0, bytes));
+									fs.close(fd);
+								}
+							);
+						}
+					);
 				}
 			);
 		}
-	);
+	});
 }
-
-
-find(function(error, data){
-	function onRequest(request, response){
-		response.writeHead(200, {'Content-type': 'text/html'});
-		response.write(data);
-		reponse.end();
-	}
-	http.createServer(onRequest).listen(port);
-})
